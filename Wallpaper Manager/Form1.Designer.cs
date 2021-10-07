d932b7936bd1b960c714ddd268f7914fba8cdc33
oilfoot <86773038+oilfoot@users.noreply.github.com>
2021-10-07 11:53:35 +0200

    Add files via upload

---
+++ Wallpaper Manager/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace Wallpaper_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.Display = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Display)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(9, 380);
+            this.btnSelect.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(259, 19);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "Select Picture";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSet
+            // 
+            this.btnSet.Location = new System.Drawing.Point(9, 403);
+            this.btnSet.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(259, 19);
+            this.btnSet.TabIndex = 1;
+            this.btnSet.Text = "Set Wallpaper";
+            this.btnSet.UseVisualStyleBackColor = true;
+            this.btnSet.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Display
+            // 
+            this.Display.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Display.Location = new System.Drawing.Point(272, 36);
+            this.Display.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(705, 393);
+            this.Display.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Display.TabIndex = 2;
+            this.Display.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(151, 46);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(116, 19);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Path:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.AcceptsReturn = true;
+            this.txtPath.AutoCompleteCustomSource.AddRange(new string[] {
+            "D:\\",
+            "C:\\",
+            "Desktop",
+            "Wallpaper",
+            "Wallpapers"});
+            this.txtPath.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.txtPath.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.txtPath.Location = new System.Drawing.Point(42, 14);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(845, 20);
+            this.txtPath.TabIndex = 6;
+            this.txtPath.TextChanged += new System.EventHandler(this.txtPath_TextChanged);
+            this.txtPath.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPath_KeyDown);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(9, 70);
+            this.webBrowser.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(15, 16);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(259, 292);
+            this.webBrowser.TabIndex = 7;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            this.webBrowser.ControlAdded += new System.Windows.Forms.ControlEventHandler(this.webBrowser_ControlAdded);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(9, 46);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(70, 19);
+            this.btnBack.TabIndex = 8;
+            this.btnBack.Text = "<<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(83, 46);
+            this.btnForward.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(64, 19);
+            this.btnForward.TabIndex = 9;
+            this.btnForward.Text = ">>";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(891, 14);
+            this.btnCheck.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(86, 19);
+            this.btnCheck.TabIndex = 10;
+            this.btnCheck.Text = "\t✓";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 432);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.btnSet);
+            this.Controls.Add(this.btnSelect);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Display)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.PictureBox Display;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ Wallpaper Manager/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Wallpaper_Manager
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        private static extern Int32 SystemParametersInfo(UInt32 action, UInt32 uParam, String vParam, UInt32 winIni);
+
+        UInt32 SPI_SETWALL = 0x14;
+        UInt32 SPIF_UPDATE = 0x01;
+        UInt32 SPIF_SWEDWINI = 0x2;
+
+        OpenFileDialog op = new OpenFileDialog();
+        string filepath;
+        string standardPath = "D:\\";
+
+        public Form1()
+        {
+            InitializeComponent();
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                webBrowser.Url = new Uri(standardPath);
+            }
+            webBrowser.DocumentCompleted += WebBrowser_DocumentCompleted;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(op.ShowDialog() == DialogResult.OK)
+            {
+                filepath = op.FileName;
+                Display.Image = Image.FromFile(filepath);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SystemParametersInfo(SPI_SETWALL, 0, filepath, SPIF_UPDATE | SPIF_SWEDWINI);
+        }
+
+
+        private void txtPath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Pfad Wählen" }) 
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    webBrowser.Url = new Uri(fbd.SelectedPath);
+                    txtPath.Text = fbd.SelectedPath;
+                }
+            };
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (webBrowser.CanGoBack)
+                webBrowser.GoBack();
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            if (webBrowser.CanGoForward)
+                webBrowser.GoForward();
+        }
+
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {                
+
+            if (webBrowser.CanSelect)
+                webBrowser.Select();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+
+                webBrowser.Url = new Uri(txtPath.Text);
+                fbd.SelectedPath = txtPath.Text;
+
+            }
+        }
+
+        private void txtPath_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+                {
+
+                    webBrowser.Url = new Uri(txtPath.Text);
+                    fbd.SelectedPath = txtPath.Text;
+
+                }
+            }
+        }
+
+        private void webBrowser_ControlAdded(object sender, ControlEventArgs e)
+        {
+            btnSelect.BackColor = Color.Aqua;
+        }
+
+        void WebBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webBrowser.Document.Body.AttachEventHandler("ondblclick", Document_DoubleClick);
+        }
+
+        void Document_DoubleClick(object sender, EventArgs e) {
+            btnSelect.BackColor = Color.Green;
+        }
+
+    }
+}
